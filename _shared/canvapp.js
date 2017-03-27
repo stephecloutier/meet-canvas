205b86e1e809730860b96acd15793246640e3853
stephecloutier <stephecloutier@gmail.com>
2017-03-27 14:04:58 +0200

    Completed canvapp

--- _shared/canvapp.js
+++ _shared/canvapp.js
@@ -1,1 +1,19 @@
 // hepl-mmi/meet-canvas - canvapp
+
+class CanvApp {
+    constructor(sCanvasID) {
+        this.canvas = document.getElementById(sCanvasID);
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        if (!window.isCanvasCompatible(this.canvas)) {
+            return console.error("Your browser doesn't support canvas. Please update.");
+        }
+        this.context = this.canvas.getContext("2d");
+    }
+
+    draw() {
+        console.warn("This method must be overrided!");
+    }
+}
+
+window.CanvApp = CanvApp;
